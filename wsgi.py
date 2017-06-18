c924a6fb1141b875a8c2c5df459e7e4a1131c7df
kdchang <kdchang.cc@gmail.com>
2017-06-18 14:33:09 +0800

    Updated wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,9 @@
 from webapp import create_app
+import os
 
 # default to dev config
 env = os.environ.get('WEBAPP_ENV', 'Prod')
 app = create_app('webapp.config.%sConfig' % env.capitalize())
 
 if __name__ == "__main__":
-    app.run()    
+    app.run(host='0.0.0.0', port='5000')    
